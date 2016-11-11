ab2b98a691a853e08992a2e8f5facee3c9090547
JPZ13 <j.p.zivalich@gmail.com>
2016-11-10 16:49:41 -0800

    Make initial folder structure

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const handlers = require('../handlers/handlers.js');
+// const server = require('http').Server(app);
+
+const app = express();
+
+const PORT = 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('client'));
+
+app.listen(PORT, () => {
+  console.log('It\'s Alive!!');
+});
