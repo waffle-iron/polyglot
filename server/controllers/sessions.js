6229d8f1324d4eb119b744d94b79e68771677c3b
JPZ13 <j.p.zivalich@gmail.com>
2016-11-14 18:58:27 -0800

    Add roulette id handling

--- server/controllers/sessions.js
+++ server/controllers/sessions.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const getPartner = require('../models/sessions').getPartner;
 
 router.post('/:language', (req, res) => {
-  // add in additional logic
-  res.sendStatus(200);
+  let resId = getPartner(req.body.user_id, req.params.language, req.body.teacher)
+  res.status(200).json(resId);
 });
 
 module.exports = router;
---
+++ server/models/sessions.js
@@ -0,0 +1,36 @@
+let langsAndIds = {
+  teachers: {},
+  students: {}
+};
+
+const getPartner = (id, language, teacher) => {
+  if (teacher) {
+    langsAndIds.students[language] = langsAndIds.students[language] || [];
+    if (langsAndIds.students[language].length > 0) {
+      return langsAndIds.students[language].shift();
+    } else {
+      langsAndIds.teachers[language] = langsAndIds.teachers[language] || [];
+      langsAndIds.teachers[language].push(id);
+      return 'OK'
+    }
+  } else {
+    langsAndIds.teachers[language] = langsAndIds.teachers[language] || [];
+    if (langsAndIds.teachers[language].length > 0) {
+      return langsAndIds.teachers[language].shift();
+    } else {
+      langsAndIds.students[language] = langsAndIds.students[language] || [];
+      langsAndIds.students[language].push(id);
+      return 'OK'
+    }
+  }
+};
+
+const resetCache = () => {
+  langsAndIds = {
+    teachers: {},
+    students: {}
+  };
+};
+
+module.exports.getPartner = getPartner;
+module.exports.resetCache = resetCache;
