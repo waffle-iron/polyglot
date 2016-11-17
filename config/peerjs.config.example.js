abf239ebdc5d3613ac4e77a2a55dc014a04413d9
J.P. Zivalich <j.p.zivalich@gmail.com>
2016-11-16 20:57:37 -0800

    Update peerjs.config.example.js

--- config/peerjs.config.example.js
+++ config/peerjs.config.example.js
@@ -1,7 +1,3 @@
 export const apiKeys = {
-<<<<<<< 86adf788de113c28007645e9a5a2b5dc37454590
   peerJs: 'Add key here'
-=======
-  peerJs: 'REPLACE_WITH_API_KEY'
->>>>>>> Make basic video chat in React
 };
