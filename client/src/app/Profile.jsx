3a6f4a13d66d69bc9c10ce090b1ad901105bfe87
Jason <jftcode@gmail.com>
2016-11-19 11:43:48 -0800

    Add profile

--- client/src/app/Profile.jsx
+++ client/src/app/Profile.jsx
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import AppBar from './AppBar';
 import Dashboard from './Dashboard';
+import SelectableLanguages from './SelectableLanguages';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import {List, ListItem} from 'material-ui/List';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import Checkbox from 'material-ui/Checkbox';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import RaisedButton from 'material-ui/RaisedButton';
+import { indigo500 } from 'material-ui/styles/colors';
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    width: '50%',
+    margin: 'auto',
+  },
+};
 
 class Profile extends Component {
   constructor(props) {
@@ -17,28 +35,38 @@ class Profile extends Component {
   render() {
 
     const learn = this.state.learnLanguages.map((lang) => {
-      return <li>{lang}</li>;
+      return <ListItem primaryText={lang} />;
     });
 
     const teach = this.state.teachLanguages.map((lang) => {
-      return <li>{lang}</li>;
+      return <ListItem primaryText={lang} />;
     });
 
     return (
       <div>
+        <AppBar />
+        <div style={styles.container}>
         <h3>Profile</h3>
-        <h4>Id</h4>
-        <input type='form'></input>
-        <input type='submit'></input>
+        <Link to='/dashboard'><RaisedButton label="Save Profile" primary></RaisedButton></Link>
+        <br />
         <h4>Learning Languages</h4>
-          <ul>
+          <List>
             {learn}
-          </ul>
+          </List>
         <h4>Teaching Languages</h4>
-          <ul>
+          <List>
             {teach}
-          </ul>
-        <Link to='/dashboard'><button>Save</button></Link>
+          </List>
+        <h4>New Language</h4>
+          <TextField
+            inputStyle={styles.container}>
+          </TextField>
+          <br />
+          <RaisedButton
+            label="Add"
+            primary
+          />
+        </div>
       </div>
     );
   }
