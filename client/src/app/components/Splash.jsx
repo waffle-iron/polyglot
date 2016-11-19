a410f2838a833b551ce55f286479f796f81f932d
El_Gato <albertomadueno@gmail.com>
2016-11-19 15:51:27 -0800

    resolve merge

--- client/src/app/components/Splash.jsx
+++ client/src/app/components/Splash.jsx
@@ -4,12 +4,7 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import { browserHistory } from 'react-router';
-<<<<<<< 24979bf44f095234f18d1a34bc8c271707d6160d:client/src/app/components/Splash.jsx
 import ApiServices from '../componentServices/ApiServices';
-=======
-// import Promise from 'redux-promise';
-import ApiServices from './componentServices/ApiServices';
->>>>>>> front end redirect on auth and serv side route connects:client/src/app/Splash.jsx
 
 const styles = {
   container: {
@@ -23,11 +18,7 @@ class Splash extends Component {
     super(props);
     this.handleEnter = this.handleEnter.bind(this);
   }
-<<<<<<< 24979bf44f095234f18d1a34bc8c271707d6160d:client/src/app/components/Splash.jsx
-
-  getUser() {
-    this.props.getUserId().then((id) => { console.log(id); } ); 
-=======
+    
   handleEnter() {
     new Promise(resolve => {
       var auth = () => {
@@ -39,16 +30,13 @@ class Splash extends Component {
     .then(()=> {
       this.props.getUserId().then((user) => { 
         var id = user.data;
-        console.log('=========', id);
         if (id > 0) {
-          console.log('then=====3')
           browserHistory.push('dashboard');
         } else {
           browserHistory.push('fourOFour');
         }
       }) 
     })
->>>>>>> front end redirect on auth and serv side route connects:client/src/app/Splash.jsx
   }
   
   render () {
